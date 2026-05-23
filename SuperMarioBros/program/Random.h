3054126ea0735b1e468aa0f68bd9fe95f3294a9b
itigomiru <Kouki190112@gmail.com>
2026-05-23 10:47:24 +0900

    Revert "Feature/develop/#00008 enemy spawner"

--- SuperMarioBros/program/Random.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-int GetRandomI( int i_max );
-int GetRandomI( int i_from, int i_to );
-
-float GetRandomF();
-float GetRandomF( float f_max );
-float GetRandomF( float f_from, float f_to );
