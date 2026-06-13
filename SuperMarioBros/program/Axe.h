7bb57abd40dbb6ea4b873f17096453648993f93c
502-30\cre <a>
2026-06-13 11:10:19 +0900

    川野/UIのコインのアニメーション実装。斧のアニメーション実装。

--- SuperMarioBros/program/Axe.h
+++ SuperMarioBros/program/Axe.h
@@ -2,9 +2,20 @@
 #include "Object.h"
 class Axe : public Object
 {
+private:
+    int animeFrame;
+    int animeTimer;
+
+    static const int AXE_FRAME_W = 16;
+    static const int AXE_FRAME_H = 16;
+    static const int AXE_FRAME_MAX = 3;
+    static const int AXE_ANIME_INTERVAL = 10;
+
 public:
     Axe();
 
     void Update(float cameraX) override;
     void Render(float cameraX) override;
+
+    
 };
